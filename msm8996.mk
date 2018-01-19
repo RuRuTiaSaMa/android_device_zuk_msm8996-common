7088c9df71e98689467a9cc12b24c8a4523a0e4d
Badhri Jagan Sridharan <Badhri@google.com>
2018-01-19 06:08:52 -0800

    mam8996-common: usb: usb gadget implementation
    (cherry-pick of commit: e9af4669659c306a9c9b1a1ae3a1313a7631357d)
    
    Bug: 63669128
    Test: Tested USB gadget configurations and verified
    that they enumerated.
    Change-Id: If0f98697488f6c7cfe335d4c292acebaaba6c20f
    
    Signed-off-by: RuRuTiaSaMa <1009087450@qq.com>

--- msm8996.mk
+++ msm8996.mk
@@ -149,7 +149,6 @@ PRODUCT_PACKAGES += \
 # Common init scripts
 PRODUCT_PACKAGES += \
     fstab.qcom \
-    init.msm.usb.configfs.rc \
     init.qcom.rc \
     init.qcom.sh \
     init.qcom.power.rc \
